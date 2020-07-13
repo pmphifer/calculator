8481f61b0612f6099889f58e46945a6fcbedc18f
Pauline <paulinekrupa@gmail.com>
2020-07-13 14:44:37 -0400

    calculator app

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //User Input
+            
+            Console.Write("Give me a number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.Write("Give me another whole number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+
+            //Sum of the numbers
+            double result = num1 + num2;
+            Console.WriteLine("The sum of your numbers is " + result);
+            Console.WriteLine();
+
+            //Difference of the numbers
+            result = num1 - num2;
+            Console.WriteLine("The difference of your numbers is " + result);
+            Console.WriteLine();
+
+            //Product of the numbers
+            result = num1 * num2;
+            Console.WriteLine($"The product of your numbers is {result}");
+            Console.WriteLine();
+
+            //Quotient of the numbers
+            result = num1 / num2;
+            Console.WriteLine($"The quotient of your numbers is {result}");
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+    }
+}
